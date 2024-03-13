9a5415a52203d417244b4514055799be418790f7
Thiago Menezes <thimenezes19@gmail.com>
2024-03-12 23:58:42 -0300

    Update views.py

--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,3 @@
-from typing import Any
-from django.db.models.query import QuerySet
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView , DetailView
 from .models import Livro
 from .forms import LivroForm
